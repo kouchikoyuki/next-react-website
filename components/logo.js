69e0181889bf40dacdeb8528c83a827981154dcc
kouchikoyuki <131890474+kouchikoyuki@users.noreply.github.com>
2023-11-24 15:42:06 +0900

    15:42

--- components/logo.js
+++ components/logo.js
@@ -1,9 +1,10 @@
 import Link from "next/link"
+import styles from 'styles/logo.module.css'
 
-export default function Logo() {
+export default function Logo({ styleType}) {
     return (
         <Link href="/">
-            CUBE
+            <div className={styles[styleType]}>CUBE</div>
         </Link>
     )
 }
