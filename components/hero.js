da9c210123ae5c52bad902ae68a122d602cdd569
kouchikoyuki <131890474+kouchikoyuki@users.noreply.github.com>
2023-11-24 16:29:59 +0900

    16:29

--- components/hero.js
+++ components/hero.js
@@ -2,7 +2,7 @@ import  style from 'styles/hero.module.scss'
 
 export default function Hero({ title, subtitle, imageOn = false }) {
     return (
-        <div>
+        <div className={StyleSheet.flexContainer}>
             <div className={style.text}>
             <h1 className={style.title}>{title}</h1>
             <p className={style.subtitle}>{subtitle}</p>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
+import Container from 'components/container'
 import Hero from '@/components/hero'
 
 export default function Hone () {
   return (
-      <Hero 
-      title="CUBE"
-      subtitle="アウトプットしていくサイト"
-      imageOn
-      />
+    <Container>
+      <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn/>
+    </Container>
   )
 }
