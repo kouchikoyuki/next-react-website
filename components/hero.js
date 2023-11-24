72a428deb456faed2720cd69e6b3271dfb78b58b
kouchikoyuki <131890474+kouchikoyuki@users.noreply.github.com>
2023-11-24 15:04:37 +0900

    15:04

--- components/hero.js
+++ components/hero.js
@@ -1,11 +1,11 @@
-import  style from 'sytles/hero.module.css'
+import  style from 'styles/hero.module.css'
 
 export default function Hero({ title, subtitle, imageOn = false }) {
     return (
         <div>
-            <div className={styles.text}>
-            <h1 className={styles.title}>{title}</h1>
-            <p className={styles.subtitle}>{subtitle}</p>
+            <div className={style.text}>
+            <h1 className={style.title}>{title}</h1>
+            <p className={style.subtitle}>{subtitle}</p>
         </div>
             {imageOn && <figure> [画像]</figure>}
         </div>
