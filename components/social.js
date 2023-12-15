e6ffb2fcb6f4b980ee1d052267020254db567180
kouchikoyuki <131890474+kouchikoyuki@users.noreply.github.com>
2023-12-15 13:57:33 +0900

    new

--- components/social.js
+++ components/social.js
@@ -1,4 +1,4 @@
-import { Styles } from 'styles/social.module.css'
+import styles from 'styles/social.module.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -9,15 +9,22 @@ import {
 
 export default function Social({ iconSize = 'initial' }) {
     return (
-        <ul className={styles.list} style={{ '--icon-size': iconSize}}>
+        <ul className={styles.list} style={{ '--icon-size' : iconSize}}>
             <li>
                 <a href='https://twitter.com/'>
+                    <FontAwesomeIcon icon={faTwitter} />
                     <span className='sr-only'>Twitter</span>
                 </a>
             </li>
             <li>
                 <a href='https://www.facebook.com/'>
                     <FontAwesomeIcon icon={faFacebookF} />
+                    <span className='sr-only'>Facebook</span>
+                </a>
+            </li>
+            <li>
+                <a href='https://github.com/'>
+                    <FontAwesomeIcon icon={faGithub} />
                     <span className='sr-only'>GitHub</span>
                 </a>
             </li>
