e3a38a37a26033300d3b15cee575eae263f462ce
kouchikoyuki <131890474+kouchikoyuki@users.noreply.github.com>
2023-12-15 16:18:41 +0900

    12151618

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,22 +1,35 @@
-import { client } from "lib/api"
+import { getPostBySlug } from "lib/api"
+import Container from "@/components/container"
+import PostHeader from "@/components/post-header"
 
-export default function Schedule() {
-    return <h1>記事のタイトル</h1>
+export default function Schedule({
+    title,
+    publish,
+    content,
+    eyecatch,
+    categories,
+}) {
+    return (
+        <Container>
+            <article>
+                <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+            </article>
+        </Container>
+    )
 }
 
 export async function getStaticProps() {
-    const resPromise = client.get({
-        endpoint: 'blogs',
-    })
+    const  slug = 'schedule' 
 
-    try {
-        const res = await resPromise
-        console.log(res)
-    } catch (err) {
-        console.log(err)
-    }
-    
+    const post = await getPostBySlug(slug)
+
     return {
-        props: {},
+        props: {
+            title: post.title,
+            publish: post.publishDate,
+            content: post.content,
+            eyecatch: post.eyecatch,
+            categories: post.categories
+        },
     }
 }
