1043ffb60cd13480e64edd49bd06c94fe95745d3
kouchikoyuki <131890474+kouchikoyuki@users.noreply.github.com>
2023-12-15 16:31:24 +0900

    saishinn

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,6 +1,7 @@
 import { getPostBySlug } from "lib/api"
 import Container from "@/components/container"
 import PostHeader from "@/components/post-header"
+import Image from "next/image"
 
 export default function Schedule({
     title,
@@ -13,23 +14,19 @@ export default function Schedule({
         <Container>
             <article>
                 <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+
+                <figure>
+                    <Image
+                    src={eyecatch.url}
+                    alt=""
+                    layout="responsive"
+                    width={eyecatch.width}
+                    height={eyecatch.height}
+                    sizes="(min-width: 1152px) 1152px, 100vw"
+                    priority
+                />
+                </figure>
             </article>
         </Container>
     )
-}
-
-export async function getStaticProps() {
-    const  slug = 'schedule' 
-
-    const post = await getPostBySlug(slug)
-
-    return {
-        props: {
-            title: post.title,
-            publish: post.publishDate,
-            content: post.content,
-            eyecatch: post.eyecatch,
-            categories: post.categories
-        },
-    }
 }
