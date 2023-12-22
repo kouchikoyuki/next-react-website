b09293925cced028946f3aaae42e9c6c01cd8805
kouchikoyuki <131890474+kouchikoyuki@users.noreply.github.com>
2023-12-22 16:24:56 +0900

    saishinn

---
+++ pages/api/revalidate.js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+    try {
+        await res.unstable_revalidate('/blog/shedule')
+        await res.unstable_revalidate('/blog/music')
+        return res.json({ revalidated: true})
+    } catch (err) {
+        return res.status(500).send('Error revalidating')
+    }
+}
