28fc1df986d7d0c7e595c22de6b7440ebe8905b1
kouchikoyuki <131890474+kouchikoyuki@users.noreply.github.com>
2024-01-19 16:30:53 +0900

    1

--- components/nav.js
+++ components/nav.js
@@ -1,9 +1,14 @@
+import { useState } from 'react'
 import Link from "next/link"
 import styles from 'styles/nav.module.css'
 
 export default function Nav() {
+    const [navIsOpen, setNavIsOpen] = useState(false)
+    
     return (
-        <nav>
+        <nav className={navIsOpen ? styles.close}>
+            <button className={styles.btn}>MENU</button>
+
             <ul className={styles.list}>
                 <li>
                     <Link href="/">
