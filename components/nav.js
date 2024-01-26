0641d37997a378ea86377b790e23772436c75cd6
kouchikoyuki <131890474+kouchikoyuki@users.noreply.github.com>
2024-01-26 15:21:21 +0900

    saishin

--- components/nav.js
+++ components/nav.js
@@ -5,24 +5,46 @@ import styles from 'styles/nav.module.css'
 export default function Nav() {
     const [navIsOpen, setNavIsOpen] = useState(false)
     
+    const toggleNav = () => {
+        setNavIsOpen((prev) => !prev)
+    }
+
+    const closeNav = () => {
+        setNavIsOpen(false)
+    }
+
     return (
-        <nav className={navIsOpen ? styles.close}>
-            <button className={styles.btn}>MENU</button>
+        <nav className={navIsOpen ? styles.open : styles.close}>
+            {navIsOpen && (
+                <style jsx global>{`
+            @media (max-width: 767px) {
+                    body {
+                        overflow: hidden;
+                        position: fixed;
+                        width: 100%;
+                    }
+                }
+            `}</style>
+    )}
+            <button className={styles.btn} onClick={toggleNav}>
+                <span className={styles.bar}></span>
+                <span className='sr-only'>MENU</span>
+            </button>
 
             <ul className={styles.list}>
                 <li>
                     <Link href="/">
-                        Home
+                        <p onClick={closeNav}>Home</p>
                     </Link>
                 </li>
                 <li>
                     <Link href="/about">
-                        About
+                        <p onClick={closeNav}>About</p>
                     </Link>
                 </li>
                 <li>
                     <Link href="/blog">
-                        Blog
+                    <p onClick={closeNav}>Blog</p>
                     </Link>
                 </li>
             </ul>
